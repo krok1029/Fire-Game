f778218fd26d5cb825f8c373557aabc7c908bf7a
krok1029 <krok1029@gmail.com>
2016-03-07 02:48:32 +0800

    按鈕完成

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class ButtonController : MonoBehaviour {
     public GameObject mesh;
-
+    public void appear()
+    {
+        mesh.SetActive(true);
+    }
+    public void disappear()
+    {
+        mesh.SetActive(false);
+    }
     }
 
