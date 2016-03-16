ce8e348d2201e3b9a8fc8543bac746b97deb18b4
Pin-Hsiu <j1152776@gmail.com>
2016-03-16 16:38:51 +0800

    加個丟臉的大象斜線走

---
+++ Assets/LineMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineMovement : MonoBehaviour {
+
+	void Update () {
+		gameObject.transform.Translate (3*Time.deltaTime, 0, 2*Time.deltaTime);
+	}
+}
---
+++ Assets/RandomMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMove : MonoBehaviour {
+	void start(){
+		InvokeRepeating("Update",1,1);
+}
+	
+
+	void Update () {
+		float i = Random.Range (-0.5F, 0.5F);
+
+
+
+		gameObject.transform.Translate (-i, 0, i);
+	}
+
+}
