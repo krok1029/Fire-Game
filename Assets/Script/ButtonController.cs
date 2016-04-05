6fba331b63887675f9ee395c03a48b266429f73d
krok1029 <krok1029@gmail.com>
2016-04-05 13:09:43 +0800

    房子拉來拉去的問題解決
    準備開始弄存檔

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -5,12 +5,15 @@ using System.Collections;
 public class ButtonController : MonoBehaviour {
     public GameObject build;
     public GameObject buildList;
-  
+    public GameObject buildbutton;
+    public GameObject backbutton;
     public Transform building;
     public void appear()
     {
         buildList.SetActive(true);
-        build.SetActive(true);        
+        build.SetActive(true);
+        backbutton.SetActive(true);
+        buildbutton.SetActive(false);
     }
 
 
@@ -18,7 +21,8 @@ public class ButtonController : MonoBehaviour {
     {
         buildList.SetActive(false);
         build.SetActive(false);
-    
+        backbutton.SetActive(false);
+        buildbutton.SetActive(true);
     }
 
 }
--- Assets/Script/dragObject.cs
+++ Assets/Script/dragObject.cs
@@ -2,48 +2,41 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class dragObject : MonoBehaviour {
+public class dragObject : MonoBehaviour
+{
     public float speed = 15;
     public Transform building ;
     public Button backbutton;
     public Button buildbutton;
-    public Button Lockbutton;
-    private bool i=true;
-    int test = 0;
-
-    void Start() {
-        backbutton.onClick.AddListener(click1);
-        buildbutton.onClick.AddListener(click2);
-        //Lockbutton.onClick.AddListener(click1);
-    }
-
+    public GameObject buildlist;
+    private bool i;
+ 
     void OnMouseDrag()
     {
+        if (buildlist.activeInHierarchy)
+        {
+            i = true;
+        }
+        else
+        {
+            i = false;
+        }
+
+
+       // Debug.Log(buildlist.activeInHierarchy);
         if (i == true)
         {
-            speed = 0;
-        }
-        else {
             speed = 15;
         }
+        else {
+            speed = 0;
+        }
+
         building.position += Vector3.right * Time.deltaTime * Input.GetAxis("Mouse X") * speed * 10;
         building.position += Vector3.forward * Time.deltaTime * Input.GetAxis("Mouse Y") * speed * 10;
-
-       
-        Debug.Log(i+ "test" + test);
-
+    
+    
     }
-
-    void click1()
-    {
-        i = true;
-        test++;
-
-    }
-    void click2()
-    {
-        i = false;
-        test++;
-    }
+    
    
 }
